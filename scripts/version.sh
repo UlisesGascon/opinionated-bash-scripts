6aa393957bdd23720a21d69f002f9720cfb136fd
Ulises Gascón <ulisesgascongonzalez@gmail.com>
2024-01-18 13:20:25 +0100

    chore: bump to 0.6.1

--- scripts/version.sh
+++ scripts/version.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 get_version () {
-    echo "Current version: 0.5.0"
-    echo "Source code: https://github.com/UlisesGascon/opinionated-bash-scripts/releases/tag/0.5.0"
+    echo "Current version: 0.6.1"
+    echo "Source code: https://github.com/UlisesGascon/opinionated-bash-scripts/releases/tag/0.6.1"
 }
