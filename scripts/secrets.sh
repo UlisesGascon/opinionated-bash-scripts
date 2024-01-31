4cfcc862835643cf4dfa54f3e9fc1d1c12d6f3e6
Ulises Gascón <ulisesgascongonzalez@gmail.com>
2024-01-31 11:00:45 +0100

    feat: add mode to load_secrets function

--- scripts/secrets.sh
+++ scripts/secrets.sh
@@ -2,11 +2,27 @@
 
 load_secrets () {
     file=${1:-.env}
-    echo "INFO: Loading secrets from $file file..."
+    mode=${2:-hard}
+    echo "INFO: Loading secrets from $file file in mode: $mode."
     if [ -f "$file" ]; then
         echo "INFO: $file file found"
-        # shellcheck disable=SC1090
-        source "$file"
+        # Load the file directly (hard mode)
+        if [ "$mode" == "hard" ]; then
+            # shellcheck disable=SC1090
+            source "$file"
+        # Load only the variables that are not already set (soft mode)
+        elif [ "$mode" == "soft" ]; then
+            # shellcheck disable=SC1090
+            while IFS='=' read -r var_name var_value; do
+                if [ -z "${!var_name}" ]; then
+                    export "$var_name"="$var_value"
+                fi
+            done < "$file"
+        else
+            echo "ERROR: Invalid mode $mode"
+            echo "SOLUTION: Please use either hard or soft mode"
+            exit 1
+        fi
         echo "OK: secrets loaded from $file file."
     else
         echo "ERROR: $file file does not exist."
